0175b00eafa564b679ecdd61be812afb8c1a4a8c
Ubugs <834602615@qq.com>
2021-10-26 12:07:38 +0800

    3-6 增加通用的返回类CommonResp, 以后所有后端接口返回值全部都是CommonResp类型

--- src/main/java/cn/ubugs/wiki/controller/EbookController.java
+++ src/main/java/cn/ubugs/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package cn.ubugs.wiki.controller;
 
 import cn.ubugs.wiki.domain.Ebook;
+import cn.ubugs.wiki.resp.CommonResp;
 import cn.ubugs.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
