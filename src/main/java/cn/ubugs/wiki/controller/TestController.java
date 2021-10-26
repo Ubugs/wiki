a40ca76ac5ae117277af165068e263bce27cc144
Ubugs <834602615@qq.com>
2021-10-26 09:34:31 +0800

    2-8 增加自定义配置项示例

--- src/main/java/cn/ubugs/wiki/controller/TestController.java
+++ src/main/java/cn/ubugs/wiki/controller/TestController.java
@@ -1,11 +1,19 @@
 package cn.ubugs.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    /**
+     * 取配置项@Value("${配置名:默认值}")
+     */
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     /**
      * Restful风格常用请求类型
      * Get Post Put Delete
@@ -14,7 +22,7 @@ public class TestController {
      */
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World, testHello:" + testHello;
     }
 
     @PostMapping("hello/post")
