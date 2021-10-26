3dda97a5b1956b08e1f67582a67741845fc408ac
Ubugs <834602615@qq.com>
2021-10-26 11:39:38 +0800

    3-5 增加了demo表列表查询

---
+++ src/main/java/cn/ubugs/wiki/controller/DemoController.java
@@ -0,0 +1,23 @@
+package cn.ubugs.wiki.controller;
+
+import cn.ubugs.wiki.domain.Demo;
+import cn.ubugs.wiki.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list() {
+        return demoService.list();
+    }
+}
---
+++ src/main/java/cn/ubugs/wiki/service/DemoService.java
@@ -0,0 +1,18 @@
+package cn.ubugs.wiki.service;
+
+import cn.ubugs.wiki.domain.Demo;
+import cn.ubugs.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
