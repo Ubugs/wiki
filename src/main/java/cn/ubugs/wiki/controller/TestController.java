7add8e276985aa7182f4011c8b2bad23f9f69891
Ubugs <834602615@qq.com>
2021-10-26 08:47:02 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/cn/ubugs/wiki/controller/TestController.java
+++ src/main/java/cn/ubugs/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package cn.ubugs.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("hello/post")
+    public String helloPost(String name) {
+        return "hello world, Post:" + name;
     }
 }
