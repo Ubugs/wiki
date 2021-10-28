ecaca38d473026eb5d31172605e00b4bfbbcdadd
Ubugs <834602615@qq.com>
2021-10-28 12:33:32 +0800

    4-7 制作the-header和the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,31 +1,24 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo"/>
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header />
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      wiki知识库 - 2021
-    </a-layout-footer>
+    <the-footer />
   </a-layout>
 </template>
 
+<script lang="ts">
+import {defineComponent} from 'vue';
+import TheHeader from '@/components/the-Header.vue';
+import TheFooter from '@/components/the-footer.vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    TheHeader,
+    TheFooter
+  },
+});
+</script>
+
 <style>
-.header .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
 </style>
